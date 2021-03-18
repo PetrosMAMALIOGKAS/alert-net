edd02b7e4237ff3505599fd8e414df815e8da2f2
Petros MAMALIOGKAS <petros.mamaliogas@gmail.com>
2021-03-18 16:32:24 +0100

    The initialisation commit of the project

---
+++ src/main/java/com/simplon/safety/alertsnet/AlertsnetApplication.java
@@ -0,0 +1,13 @@
+package com.simplon.safety.alertsnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlertsnetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlertsnetApplication.class, args);
+	}
+
+}
